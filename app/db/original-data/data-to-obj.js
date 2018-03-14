26715619c9cd6b2318a70d53015b4e3081440a63
Z Yin <Z.Yin.MD@gmail.com>
2018-03-14 16:04:39 -0400

    partially changed to async, but the main heavy lifting is still sync

--- app/db/original-data/data-to-obj.js
+++ app/db/original-data/data-to-obj.js
@@ -1,11 +1,17 @@
+const dataInput = './mock2012-2016/'; // a folder name
+const dataOutput = '../mockdata.json'; // a file
 const fs = require('fs');
-var fileList = fs.readdirSync('./mock2012-2016/');
 var database = {}; // this'll be where all the data are stored
-for (let i of fileList) {
-  let year = i.slice(3, 7);
-  let thisYearsContent = fs.readFileSync('./mock2012-2016/' + i, 'UTF-8');
-  processThisYear(year, thisYearsContent);
-}
+
+fs.readdir(dataInput, (err, fileList) => {
+  if (err) throw err;
+  for (let i of fileList) {
+    let year = i.slice(3, 7);
+    let thisYearsContent = fs.readFileSync('./mock2012-2016/' + i, 'UTF-8');
+    processThisYear(year, thisYearsContent);
+  }
+  fs.writeFileSync(dataOutput, JSON.stringify(database, null, 1)); //final output
+});
 
 function processThisYear(year, thisYearsContent) {
   var lines = thisYearsContent.split('\n');
@@ -21,5 +27,4 @@ function processThisLine(year, name, gender, population) {
   database[name][year] = Number(population);
 }
 
-fs.writeFileSync('../mockdata.json', JSON.stringify(database, null, 1));
 
