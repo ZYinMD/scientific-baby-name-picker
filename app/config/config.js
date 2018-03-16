0e1fba26856e1f1efbd61eb8da553ad3fc83fc3a
Z Yin <Z.Yin.MD@gmail.com>
2018-03-15 23:43:18 -0400

    gitignore test

--- app/config/config.js
+++ app/config/config.js
@@ -1,29 +1,9 @@
-/*const Sequelize = require('sequelize');
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql'|'sqlite'|'postgres'|'mssql',
-  operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-
-});
-
-// Or you can simply use a connection uri
-// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
-*/
 var config = {
-  meta:{
-    instructions: "If you need to change your localhost mysql server password, don't change it here, it'll get pushed. Edit the .env in root"
-  }
   development: {
     database: 'baby_name_picker',
     username: 'root',
-    password: null,
+    password: process.env.LOCALHOST_MYSQL_SERVER_ROOT_PASSWORD||null, // If you need to change your localhost mysql server password, don't change it here, it'll get pushed. Edit the .env in root
     settings: {
       host: 'localhost',
       dialect: 'mysql',
@@ -51,5 +31,4 @@ var config = {
     }
   }
 };
-
 require('fs').writeFileSync('config.json', JSON.stringify(config, null, 2));
--- app/models/connection.js
+++ app/models/connection.js
@@ -1,6 +1,6 @@
-// const Sequelize = require("sequelize");
+const Sequelize = require("sequelize");
 const enviroment = process.env.NODE_ENV || 'development';
-// var config = require('/../config/config.json')[env];
+var config = require('../config/config.json')[enviroment];
 require('dotenv').config();
-console.log(enviroment);
 console.log(process.env.LOCALHOST_MYSQL_SERVER_ROOT_PASSWORD);
+
