6ac661d4c5ce00116593435ca8efdc8ac57c2117
Z Yin <Z.Yin.MD@gmail.com>
2018-03-18 11:07:15 -0400

    JawsDB test run

--- app/config/config.js
+++ app/config/config.js
@@ -1,21 +1,29 @@
-// this file contains settings to connect to the database
+// This file contains settings to connect to the database
+
 require('dotenv').config();
-module.exports = {
-  development: {
-    host: 'localhost',
+const development = {
+    host: 'localhost', //this line is optional as it's the default value
+    port: 3306, //this line is optional as it's the default value
+    database: 'baby_name_picker',
     user: 'root',
+    password: process.env.LOCALHOST_MYSQL_SERVER_ROOT_PASSWORD||null,
     // If you need to set your localhost mysql server password, either change the null part, or create your own .env file.
     // Note: if the dotenv is used, keep in mind it always looks for the .env file in the cwd where you launch node.
-    password: process.env.LOCALHOST_MYSQL_SERVER_ROOT_PASSWORD||null,
-  },
-  production: {
-    host: 'example.org',
-    user: 'you',
-    password: 'secret'
-  }
-}
+  };
 
-// The code below is no longer used. It was once used for npm sequelize. But I've switched npm mysql instead.
+module.exports = process.env.JAWSDB_URL || development;
+
+
+
+
+
+
+
+
+
+
+// The code below is no longer used. It was once used for npm sequelize, but I've switched npm mysql instead.
+
 /*
 module.exports = {
   development: {
