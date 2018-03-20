e3c8ff42b23b85bcfed9a3a5d6633e36f8078dd9
Z Yin <Z.Yin.MD@gmail.com>
2018-03-19 23:47:04 -0400

    filter hide/unhide done

--- app/public/scripts/frontend.js
+++ app/public/scripts/frontend.js
@@ -1,21 +1,34 @@
-$('#test-query').on('submit', (e) => {
-  e.preventDefault();
+initialize();
+function initialize() { // this function get run on page load
+// temp: enter queries in the query bar to run a query
+$('#test-query').on('submit', (event) => {
+  event.preventDefault();
   var query = ($('#test-query__input').val().trim());
-  $.get('/api', {query: query}, populateResult);
+  $.get('/api', {query: query}, populateNames);
 });
 
-function populateResult(res) {
+
+  // filter behavior:
+  $('.filter-row:not(.filter-col--a p)').toggleClass('hidden', true); //hide everything except first column
+  $('.filter-col--a').on('change', 'input', (event) => { // when any filter is chosen, unhide its row
+    unhide(event.target.parentNode.classList[1]);
+  });
+}
+
+function unhide(row) {
+  $('.filter-row:not(.filter-col--a p)').toggleClass('hidden', true); //hide everything except first column
+  $('.filter-col--b p').toggleClass('hidden', false); //unhide column b
+  $('.' + row).toggleClass('hidden', false); // unhide the selected row
+
+}
+
+function populateNames(res) { // this function populates the screen the server res
   var display = '';
   for (let i in res) {
     display += `result #${i}: ${JSON.stringify(res[i])}\n`;
   }
-
-
-  $('#mockup').empty().append($('<pre>'));
-
+  $('#name-list').empty().append($('<pre>'));
   $('pre').text(display);
 }
 
-//this temporary function format the res from server
-function formatRes(res) {
-}
+
