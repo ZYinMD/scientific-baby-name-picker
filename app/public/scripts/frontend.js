f568e37b331b90441e3927672445ae0290149d84
Z Yin <Z.Yin.MD@gmail.com>
2018-03-22 17:19:06 -0400

    fixed test-query for team members to try

--- app/public/scripts/frontend.js
+++ app/public/scripts/frontend.js
@@ -1,64 +1,10 @@
-pageInitialize();
-materializeInitialize();
+pageInit();
+materializeInit();
+slidersInit();
+testQueryInit();
 
-
-
-
-
-
-var slider = document.getElementById('year-slider');
-noUiSlider.create(slider, {
- start: [1950, 2016],
- connect: true,
- step: 1,
- direction: 'rtl', // smaller values first
- orientation: 'vertical', // 'horizontal' or 'vertical'
- range: {
-   'min': 1880,
-   'max': 2016
- },
- format: wNumb({
-   decimals: 0
- })
-});
-
-
-// var slider = document.getElementById('c-popular__range');
-// var output = document.getElementById("c-popular--value");
-// output.innerHTML = slider.value; // display the default slider value
-// slider.oninput = function() { // update the displayed value on dragging
-//     output.innerHTML = this.value;
-// };
-
-
-var startYear = document.getElementById('start-year');
-var endYear = document.getElementById('end-year');
-slider.noUiSlider.on('update', () => {
-  [startYear.innerHTML, endYear.innerHTML] = slider.noUiSlider.get();
-  });
-
-
-
-
-
-
-
-
-
-
-
-function materializeInitialize() {
-  $('.collapsible').collapsible();
-}
-
-
-function pageInitialize() { // this function get run on page load
-// temp: enter queries in the query bar to run a query
-$('#test-query').on('submit', (event) => {
-  event.preventDefault();
-  var query = ($('#test-query__input').val().trim());
-  $.get('/api', {query: query}, populateNames);
-});
+function pageInit() { // this function get run on page load
+  // temp: enter queries in the query bar to run a query
 
   // change text color of selected radio
   $('#filter-console input').on('change', (event) => {
@@ -66,33 +12,20 @@ $('#test-query').on('submit', (event) => {
     console.log('event.target.parentNode.parentNode: ', event.target.parentNode.parentNode);
     console.log('event.target.checked: ', event.target.checked);
   });
-
   $('.filter-row').on('click', () => {
     console.log('event.currentTarget', event.currentTarget);
   });
-
   // filter hide/show:
   $('.filter-row:not(.filter-col__a li)').hide(); //hide everything except first column
   $('.filter-col__a').on('change', 'input', (event) => { // when any filter is chosen, unhide its row
     unhide(event.target.parentNode.parentNode.classList[1]);
   });
-
-  // for the one and only html range in the app
-  var slider = document.getElementById('c-popular__range');
-  var output = document.getElementById("c-popular--value");
-  output.innerHTML = slider.value; // display the default slider value
-  slider.oninput = function() { // update the displayed value on dragging
-      output.innerHTML = this.value;
-  };
-
-
 }
 
 function unhide(row) {
   $('.filter-row:not(.filter-col__a li)').hide(); //hide everything except first column
   $('.filter-col__b li').show(); //unhide column b
   $('.' + row).show(); // unhide the selected row
-
 }
 
 function populateNames(res) { // this function populates the screen the server res
@@ -104,4 +37,47 @@ function populateNames(res) { // this function populates the screen the server r
   $('pre').text(display);
 }
 
+function slidersInit() {
+  // the year range slider, using noUiSlider
+  var slider = document.getElementById('year-slider');
+  noUiSlider.create(slider, {
+    start: [1950, 2016],
+    connect: true,
+    step: 1,
+    direction: 'rtl', // smaller values first
+    orientation: 'vertical', // 'horizontal' or 'vertical'
+    range: {
+      'min': 1880,
+      'max': 2016
+    },
+    format: wNumb({
+      decimals: 0
+    })
+  });
+  var startYear = document.getElementById('start-year');
+  var endYear = document.getElementById('end-year');
+  slider.noUiSlider.on('update', () => {
+    [startYear.innerHTML, endYear.innerHTML] = slider.noUiSlider.get();
+  });
+}
 
+function materializeInit() {
+  $('.collapsible').collapsible();
+  // for the one and only html range in the app
+  var slider = document.getElementById('c-popular__range');
+  var output = document.getElementById("c-popular--value");
+  output.innerHTML = slider.value; // display the default slider value
+  slider.oninput = function() { // update the displayed value on dragging
+    output.innerHTML = this.value;
+  };
+}
+
+function testQueryInit() {
+  $('#test-query').on('submit', (event) => {
+    event.preventDefault();
+    var query = ($('#test-query__input').val().trim());
+    $.get('/api', {
+      query: query
+    }, populateNames);
+  });
+}
